d59c9c60831a69e2780b501bf0701b4b6fca3aae
Anurag Sinha <sinhaanurag18@gmail.com>
2019-12-08 18:38:05 +0530

    OAuth login using Github Account

---
+++ src/test/java/io/anurag/springbootstarter/LoginWithFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anurag.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithFacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
